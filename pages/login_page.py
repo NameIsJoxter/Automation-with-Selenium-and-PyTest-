d2eecc9ed946a142079da42b2f68933e35709fb7
Galina Detinich <quellik@gmail.com>
2019-10-16 18:44:34 +0300

    Login page description small refinement

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,14 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import MainPageLocators, LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-        # self.should_be_login_url()
+        self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    # def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        # assert True
+    def should_be_login_url(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not valid'
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
