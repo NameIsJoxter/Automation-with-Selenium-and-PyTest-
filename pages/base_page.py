da72a99041f4ca1d110042fbcce3667fb7d5aad9
Galina Detinich <quellik@gmail.com>
2019-10-16 13:55:06 +0300

    Add exception handling

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,15 @@
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
+        self.browser.implicitly_wait(timeout)
         self.url = url
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except Exception:
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
