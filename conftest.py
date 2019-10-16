47fba6d9c56651fbf147aaa221bf0af4f38a3bc3
Galina Detinich <quellik@gmail.com>
2019-10-16 18:40:00 +0300

    Browser is now known as driver, plus some small refinements

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def driver(request):
-    user_language= request.config.getoption('language')
+    user_language = request.config.getoption('language')
     options = Options()
     options.add_experimental_option('prefs',{'intl.accept_languages': user_language})
     print(f"\nStart browser for test, {user_language} language")
--- pages/base_page.py
+++ pages/base_page.py
@@ -2,31 +2,33 @@ import math
 from selenium.common.exceptions import NoAlertPresentException
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
-        self.browser = browser
-        self.browser.implicitly_wait(timeout)
+
+    def __init__(self, driver, url, timeout=5):
+        self.driver = driver
+        self.driver.implicitly_wait(timeout)
         self.url = url
 
     def is_element_present(self, how, what):
         try:
-            self.browser.find_element(how, what)
+            self.driver.find_element(how, what)
         except Exception:
             return False
         return True
 
     def open(self):
-        self.browser.get(self.url)
+        self.driver.get(self.url)
 
     def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
+        alert = self.driver.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
         try:
-            alert = self.browser.switch_to.alert
+            alert = self.driver.switch_to.alert
             alert_text = alert.text
             print(f"Your code: {alert_text}")
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+        self.driver.sleep(30000)
