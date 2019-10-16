6c7b3552edfde0b1c415299c160d21ee4b9203fd
Galina Detinich <quellik@gmail.com>
2019-10-16 14:43:23 +0300

    Refine test guest can go to login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 # import pytest
 # from selenium.common.exceptions import NoSuchElementException
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,6 +9,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage (browser, browser.current_url)
+    login_page.should_be_login_page()
 
     # with pytest.raises(NoSuchElementException):
     #     page.should_be_login_link()
