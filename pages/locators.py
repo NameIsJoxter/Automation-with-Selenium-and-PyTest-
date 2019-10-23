9b468485cf75c1cf52109e931b4e0a474ee7fada
Galina Detinich <quellik@gmail.com>
2019-10-23 18:03:51 +0300

    Add register form locators at login page

--- pages/locators.py
+++ pages/locators.py
@@ -19,7 +19,10 @@ class LoginPageLocators:
     LOGIN_URL = (driver.current_url, 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/')     # WTD: get rid of hardcode for en-gb
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
-
+    EMAIL_REGISTRATION = (By.CSS_SELECTOR, '#id_registration-email')
+    PASSWORD_REGISTRATION = (By.CSS_SELECTOR, '#id_registration-password1')
+    PASSWORD_REGISTRATION_CONFIRM = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[name=registration_submit]')
 
 class ProductPageLocators:
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
